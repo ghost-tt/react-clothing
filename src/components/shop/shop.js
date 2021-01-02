a949c3af27ae2141915d0f05fc115e7b3920fe0a
Ashish <ashishbhardwaj727@gmail.com>
2021-01-03 01:44:04 +0530

    - advance routing - display collection based on categoryId from url

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import CollectionsOverview from '../collection-overview/collection-overview';
+import { Route } from 'react-router-dom';
 
-const Shop = () => (
+import CollectionsOverview from '../collection-overview/collection-overview';
+import CollectionPage from '../../pages/collection/collection';
+
+const Shop = ({match}) => (
     <div className="shop-page">
-        <CollectionsOverview />
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:categoryId`} component={CollectionPage} />
     </div>
 );
 
